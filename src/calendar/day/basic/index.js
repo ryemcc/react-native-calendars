b0641dd5f618a20d133624a50c6b9317567357eb
Ryan McConnell <ryan.mcconnell@salesforce.com>
2019-07-25 11:15:22 -0700

    change assignment of day to fix rerender error

--- src/calendar/day/basic/index.js
+++ src/calendar/day/basic/index.js
@@ -1,100 +1,106 @@
-import React, {Component} from 'react';
-import {
-  TouchableOpacity,
-  Text,
-  View
-} from 'react-native';
-import PropTypes from 'prop-types';
-import {shouldUpdate} from '../../../component-updater';
+import React, { Component } from 'react'
+import { TouchableOpacity, Text, View } from 'react-native'
+import PropTypes from 'prop-types'
+import { shouldUpdate } from '../../../component-updater'
 
-import styleConstructor from './style';
+import styleConstructor from './style'
 
 class Day extends Component {
-  static displayName = 'IGNORE';
-  
-  static propTypes = {
-    // TODO: disabled props should be removed
-    state: PropTypes.oneOf(['disabled', 'today', '']),
+	static displayName = 'IGNORE'
 
-    // Specify theme properties to override specific styles for calendar parts. Default = {}
-    theme: PropTypes.object,
-    marking: PropTypes.any,
-    onPress: PropTypes.func,
-    onLongPress: PropTypes.func,
-    date: PropTypes.object
-  };
+	static propTypes = {
+		// TODO: disabled props should be removed
+		state: PropTypes.oneOf(['disabled', 'today', '']),
 
-  constructor(props) {
-    super(props);
-    this.style = styleConstructor(props.theme);
-    this.onDayPress = this.onDayPress.bind(this);
-    this.onDayLongPress = this.onDayLongPress.bind(this);
-  }
+		// Specify theme properties to override specific styles for calendar parts. Default = {}
+		theme: PropTypes.object,
+		marking: PropTypes.any,
+		onPress: PropTypes.func,
+		onLongPress: PropTypes.func,
+		date: PropTypes.object
+	}
 
-  onDayPress() {
-    this.props.onPress(this.props.date);
-  }
-  onDayLongPress() {
-    this.props.onLongPress(this.props.date);
-  }
+	constructor(props) {
+		super(props)
+		this.style = styleConstructor(props.theme)
+		this.onDayPress = this.onDayPress.bind(this)
+		this.onDayLongPress = this.onDayLongPress.bind(this)
+	}
 
-  shouldComponentUpdate(nextProps) {
-    return shouldUpdate(this.props, nextProps, ['state', 'children', 'marking', 'onPress', 'onLongPress']);
-  }
+	onDayPress() {
+		this.props.onPress(this.props.date)
+	}
+	onDayLongPress() {
+		this.props.onLongPress(this.props.date)
+	}
 
-  render() {
-    const containerStyle = [this.style.base];
-    const textStyle = [this.style.text];
-    const dotStyle = [this.style.dot];
+	shouldComponentUpdate(nextProps) {
+		return shouldUpdate(this.props, nextProps, [
+			'state',
+			'children',
+			'marking',
+			'onPress',
+			'onLongPress'
+		])
+	}
 
-    let marking = this.props.marking || {};
-    if (marking && marking.constructor === Array && marking.length) {
-      marking = {
-        marking: true
-      };
-    }
-    const isDisabled = typeof marking.disabled !== 'undefined' ? marking.disabled : this.props.state === 'disabled';
-    let dot;
-    if (marking.marked) {
-      dotStyle.push(this.style.visibleDot);
-      if (isDisabled) {
-        dotStyle.push(this.style.disabledDot);
-      }
-      if (marking.dotColor) {
-        dotStyle.push({backgroundColor: marking.dotColor});
-      }
-      dot = (<View style={dotStyle}/>);
-    }
+	render() {
+		const containerStyle = [this.style.base]
+		const textStyle = [this.style.text]
+		const dotStyle = [this.style.dot]
 
-    if (marking.selected) {
-      containerStyle.push(this.style.selected);
-      if (marking.selectedColor) {
-        containerStyle.push({backgroundColor: marking.selectedColor});
-      }
-      dotStyle.push(this.style.selectedDot);
-      textStyle.push(this.style.selectedText);
-    } else if (isDisabled) {
-      textStyle.push(this.style.disabledText);
-    } else if (this.props.state === 'today') {
-      containerStyle.push(this.style.today);
-      textStyle.push(this.style.todayText);
-      dotStyle.push(this.style.todayDot);
-    }
+		let marking = this.props.marking || {}
+		if (marking && marking.constructor === Array && marking.length) {
+			marking = {
+				marking: true
+			}
+		}
+		const isDisabled =
+			typeof marking.disabled !== 'undefined'
+				? marking.disabled
+				: this.props.state === 'disabled'
+		let dot = <View style={dotStyle} />
+		if (marking.marked) {
+			dotStyle.push(this.style.visibleDot)
+			if (isDisabled) {
+				dotStyle.push(this.style.disabledDot)
+			}
+			if (marking.dotColor) {
+				dotStyle.push({ backgroundColor: marking.dotColor })
+			}
+		}
 
-    return (
-      <TouchableOpacity
-        testID={this.props.testID}
-        style={containerStyle}
-        onPress={this.onDayPress}
-        onLongPress={this.onDayLongPress}
-        activeOpacity={marking.activeOpacity}
-        disabled={marking.disableTouchEvent}
-      >
-        <Text allowFontScaling={false} style={textStyle}>{String(this.props.children)}</Text>
-        {dot}
-      </TouchableOpacity>
-    );
-  }
+		if (marking.selected) {
+			containerStyle.push(this.style.selected)
+			if (marking.selectedColor) {
+				containerStyle.push({ backgroundColor: marking.selectedColor })
+			}
+			dotStyle.push(this.style.selectedDot)
+			textStyle.push(this.style.selectedText)
+		} else if (isDisabled) {
+			textStyle.push(this.style.disabledText)
+		} else if (this.props.state === 'today') {
+			containerStyle.push(this.style.today)
+			textStyle.push(this.style.todayText)
+			dotStyle.push(this.style.todayDot)
+		}
+
+		return (
+			<TouchableOpacity
+				testID={this.props.testID}
+				style={containerStyle}
+				onPress={this.onDayPress}
+				onLongPress={this.onDayLongPress}
+				activeOpacity={marking.activeOpacity}
+				disabled={marking.disableTouchEvent}
+			>
+				<Text allowFontScaling={false} style={textStyle}>
+					{String(this.props.children)}
+				</Text>
+				{dot}
+			</TouchableOpacity>
+		)
+	}
 }
 
-export default Day;
+export default Day
